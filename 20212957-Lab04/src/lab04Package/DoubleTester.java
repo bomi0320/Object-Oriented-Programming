1a97b63f1130a086241220d57c0b210f3f4fdba0
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2021-10-13 16:30:28 +0900

    Update DoubleTester.java

--- 20212957-Lab04/src/lab04Package/DoubleTester.java
+++ 20212957-Lab04/src/lab04Package/DoubleTester.java
@@ -5,7 +5,7 @@ public class DoubleTester {
 	public static void main(String[] args) {
 
 		double probability = 8.70;
-		int percentage = (int)Math.round(100 * probability);
+		int percentage = (int) (Math.round(100 * probability)); // Math.round()메쏘드를 사용해서 소수 첫째 자리에서 반올림함.
 		System.out.println(percentage);
 	}
 
